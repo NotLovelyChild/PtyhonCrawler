bcbbffeca3c8ee6bbb88d40f858fc11c672292fc
ChenZhe <chenzhe@miliantech.com>
2019-11-03 23:18:28 +0800

    1

--- crawler/C/DM36.py
+++ crawler/C/DM36.py
@@ -2,13 +2,23 @@ import Config
 
 def search(name):
 	data=[]
-	#http://www.36dm.club/show-45ddaab867fe81485a098beb982a7b8026b4ec05.html
 	url="http://www.36dm.club/search.php?keyword="+name
 	soup=Config.requestUrlWithChrome(url)
 	items=soup.select(".alt1")+soup.select(".alt2")
 	for item in items:
-		print(item)
+		a=item.select("a")
+		if len(a)>1:
+			title=a[1].text
+			href="https://www.36dm.club/"+a[1]["href"]
+			print(title,href)
 
+def get_magnet(url):
+	soup=Config.requestUrlWithChrome(url)
+	magnet=soup.select("#magnet")
+	if len(magnet):
+		return magnet[0]['href']
 
 if __name__ == "__main__":
-	search("鬼灭之刃")
+#	search("鬼灭之刃")
+	url="https://www.36dm.com/show-c61a99ab29570ae62b61782f7647605f3b3af5d9.html"
+	print(get_magnet(url))
